e0e45e2f9d07e542c0d6309907f662b1ed57cbd2
PiotrekCode <Lukawski.piotrek@zst.radom.pl>
2025-03-31 00:05:54 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+
 import React, { useState, useEffect } from "react";
-import './App.css'; // Poprawny import pliku CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const KLUCZ_API = "49609126-94ba6cbc926165059a77973b0"; 
 const KATEGORIE = ["samochody", "natura", "zwierzęta"];
